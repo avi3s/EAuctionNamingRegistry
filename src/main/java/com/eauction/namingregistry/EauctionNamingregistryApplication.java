c18c44372b7d65584a8d2b886e11fdb31cc873b1
avi3s <avirup.pal@gmail.com>
2022-08-23 11:36:03 +0530

    first commit

---
+++ src/main/java/com/eauction/namingregistry/EauctionNamingregistryApplication.java
@@ -0,0 +1,13 @@
+package com.eauction.namingregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EauctionNamingregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EauctionNamingregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eauction/namingregistry/EauctionNamingregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eauction.namingregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EauctionNamingregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
