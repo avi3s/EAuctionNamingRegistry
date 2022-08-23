70e2a3a7fc7e1087a15c51581fcf25c47eecf2ce
avi3s <avirup.pal@gmail.com>
2022-08-23 18:51:34 +0530

    Naming Registry Updated

--- src/main/java/com/eauction/namingregistry/EauctionNamingregistryApplication.java
+++ src/main/java/com/eauction/namingregistry/EauctionNamingregistryApplication.java
@@ -2,12 +2,25 @@ package com.eauction.namingregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EauctionNamingregistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EauctionNamingregistryApplication.class, args);
 	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+		http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		http.headers().frameOptions().sameOrigin();
+
+		return http.build();
+	}
 }
